14c2111c03aedb4f4bdfe834c369d91de5270941
Felipe Francisco <fefarias97@gmail.com>
2021-11-21 00:11:39 -0300

    Resolução exercício senha

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string SENHA = "1234abcd";
+string senhaDigitada,acesso;
+
+Console.Write("Olá usuário, digite a sua senha: ");
+senhaDigitada = Console.ReadLine();
+
+acesso = senhaDigitada == SENHA ? "Permitido":"Negado";
+
+if(senhaDigitada == SENHA){
+    Console.WriteLine($"OK. Acesso {acesso}.");
+}else{
+    Console.WriteLine($"Lamentamos. Acesso {acesso}.");
+}
+
